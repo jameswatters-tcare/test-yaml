0d9f90d1faa4c41a3cee6566ad1843114122124d
James Watters <james.watters@tcare.ai>
2023-10-06 11:28:26 -0500

    After trying secrets

--- tests/e2e/test-basic.spec.ts
+++ tests/e2e/test-basic.spec.ts
@@ -1,7 +1,8 @@
 import { expect, test } from '@playwright/test';
 
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected h1', async ({ page }, testInfo) => {
+	console.log(testInfo.project.name.toUpperCase());
 	await page.goto('/');
 	// await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Hello world!' })).toBeVisible();
