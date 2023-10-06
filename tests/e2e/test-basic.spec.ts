827d0445f85b7fd109ab7e55a017cf74a4dd63ef
James Watters <james.watters@tcare.ai>
2023-10-06 11:41:19 -0500

    Test again w/ env

--- tests/e2e/test-basic.spec.ts
+++ tests/e2e/test-basic.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from '@playwright/test';
 
 
 test('index page has expected h1', async ({ page }, testInfo) => {
+	console.log("Project name from test");
 	console.log(testInfo.project.name.toUpperCase());
 	await page.goto('/');
 	// await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
